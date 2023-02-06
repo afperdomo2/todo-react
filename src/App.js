8e6d064a873e760584998ff4427191e07e2fd2f8
alvarado0925 <alvarado0925@gmail.com>
2023-02-05 23:34:41 -0500

    🚀Refactor: Elimina archivos que no se van a usar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 const urlPlatzi = 'https://platzi.com/';
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {props.children}
-        {props.despedida}
-        <a
-          className="App-link"
-          href={urlPlatzi}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React con Platzi
-        </a>
-      </header>
-    </div>
-  );
+function App() {
+  return ();
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+//import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App despedida="Adios gente">
-    <h1>Hola a todos.</h1>
-  </App>
+  <App />
 );
