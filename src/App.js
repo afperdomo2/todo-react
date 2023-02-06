7669de4db71d7db92027f8f8e63400432154d4d5
alvarado0925 <alvarado0925@gmail.com>
2023-02-06 00:20:14 -0500

    🧪Feat: Crea los componentes bases del proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from "react";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import { TodoItem } from "./components/TodoItem";
+import { CreateTodoButton } from "./components/CreateTodoButton";
 //import './App.css';
 
-const urlPlatzi = 'https://platzi.com/';
+const todos = [
+  { text: 'Hace mercado', completed: false },
+  { text: 'Pasear al perro', completed: false },
+  { text: 'Cortar manzana', completed: false },
+];
 
 function App() {
-  return ();
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map((todo, index) => (
+          <TodoItem key={index} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoCounter() {
+    return (
+        <h2>Has comeplado 1 de 5 tareas</h2>
+    );
+}
+
+export { TodoCounter };
